b8e9546637f62c611c347fd71a0e73e9054cdcc9
Sípos Szabolcs <siposszabolcs10@gmail.com>
2024-09-16 12:17:29 +0200

    Added a way to logout and go back from dashboard to trainer selection screen

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSignOut } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  faSignOut = faSignOut;
+
+  router = inject(Router);
+
+  handleClick(): void {
+    const url = this.router.url;
+    const endpoint = this.extractEndpoint(url);
+
+    if (endpoint === 'trainers') {
+      this.router.navigateByUrl('');
+    } else if (endpoint === 'dashboard') {
+      this.router.navigateByUrl('trainers');
+    }
+  }
+
+  extractEndpoint(url: string): string {
+    const parts = url.split('/');
+    return parts[parts.length - 1];
+  }
+}
