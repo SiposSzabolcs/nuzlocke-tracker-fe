81afae9b9ade5a719e6ff36c70cc6f7595ab5ec8
Sípos Szabolcs <siposszabolcs10@gmail.com>
2024-09-18 10:18:03 +0200

    fixed creating trainer

--- src/app/pages/trainers/new-trainer/new-trainer.component.ts
+++ src/app/pages/trainers/new-trainer/new-trainer.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { catchError, of, switchMap } from 'rxjs';
+import { catchError, firstValueFrom, of, switchMap, tap } from 'rxjs';
 
 import { Router, RouterModule } from '@angular/router';
 import { TrainerService } from '../../../services/trainer-service/trainer.service';
@@ -27,13 +27,16 @@ export class NewTrainerComponent {
 
   getTrainers() {
     const email = this.trainerService.getEmailFromToken();
+    console.log(email);
     const payload = { email };
 
-    return this.http
+    this.http
       .post<{ id: string }>('http://localhost:8080/users/email', payload)
       .pipe(
         switchMap((res: { id: string }) => {
+          console.log(res);
           this.id = res.id;
+          console.log(this.id);
           return of(res);
         }),
         catchError((error) => {
@@ -43,16 +46,46 @@ export class NewTrainerComponent {
       );
   }
 
-  handleClick() {
+  async getId() {
+    const email = this.trainerService.getEmailFromToken();
+    console.log(email);
+    const payload = { email };
+
+    try {
+      const response = await firstValueFrom(
+        this.http
+          .post<{ id: string }>('http://localhost:8080/users/email', payload)
+          .pipe(
+            catchError((error) => {
+              console.error('Error fetching userId:', error);
+              return of(null);
+            })
+          )
+      );
+      return response ? response.id : null;
+    } catch (error) {
+      console.error('Error in getId:', error);
+      return null;
+    }
+  }
+
+  async handleClick() {
+    let userId = await this.getId();
+    console.log(userId);
+
     const trainer = {
       name: this.trainerForm.value.name || '',
-      userId: this.id,
+      userId,
       routeIds: this.routeService.kanto_routes,
+      game: 'firered',
     };
+
+    console.log(trainer);
 
     this.http
       .post(`http://localhost:8080/trainers`, trainer)
       .subscribe((res: any) => {
+        console.log(res);
         this.trainerService.setTrainerId(res.id);
         localStorage.setItem('lastTrainerId', `${res.id}`);
         this.router.navigateByUrl('dashboard');
